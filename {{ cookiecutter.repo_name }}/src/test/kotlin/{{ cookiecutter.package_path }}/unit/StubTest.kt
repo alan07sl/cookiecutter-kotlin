89a8d15ea288fefe0b6b6f9d9ab67b42da9c5081
Alan Hryniewicz <alan.hryniewicz@mercadolibre.com>
2020-06-15 00:31:49 -0300

    Add code cov

---
+++ {{ cookiecutter.repo_name }}/src/test/kotlin/{{ cookiecutter.package_path }}/unit/StubTest.kt
@@ -0,0 +1,11 @@
+package {{ cookiecutter.package_name }}.unit
+
+import org.junit.jupiter.api.Test
+
+class StubTest {
+
+    @Test
+    fun stubTest() {
+        // Tests nothing should always pass
+    }
+}
